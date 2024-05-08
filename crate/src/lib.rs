22079a7465e197a667f59957ae30ddcc9ac91a29
Bob McWhirter <bmcwhirt@redhat.com>
2024-05-08 14:27:08 -0400

    Provide a wrapper Rust crate around the static-built UI.

---
+++ crate/src/lib.rs
@@ -0,0 +1,103 @@
+include!(concat!(env!("OUT_DIR"), "/generated.rs"));
+
+use std::cell::OnceCell;
+use std::collections::HashMap;
+use std::fs;
+use std::str::from_utf8;
+use std::sync::{Arc, Mutex, OnceLock};
+use base64::Engine;
+use base64::prelude::BASE64_STANDARD;
+use serde_json::Value;
+use static_files::Resource;
+use static_files::resource::new_resource;
+use serde::Serialize;
+
+#[derive(Serialize, Clone, Default)]
+pub struct UI {
+    #[serde(rename(serialize = "VERSION"))]
+    pub version: String,
+
+    #[serde(rename(serialize = "AUTH_REQUIRED"))]
+    pub auth_required: String,
+
+    #[serde(rename(serialize = "OIDC_SERVER_URL"))]
+    pub oidc_server_url: String,
+
+    #[serde(rename(serialize = "OIDC_CLIENT_ID"))]
+    pub oidc_client_id: String,
+
+    #[serde(rename(serialize = "OIDC_SCOPE"))]
+    pub oidc_scope: String,
+
+    #[serde(rename(serialize = "ANALYTICS_ENABLED"))]
+    pub analytics_enabled: String,
+
+    #[serde(rename(serialize = "ANALYTICS_WRITE_KEY"))]
+    pub analytics_write_key: String,
+}
+
+pub fn trustify_ui_resources() -> HashMap<&'static str, Resource> {
+    let mut resources = generate();
+    if let Some(index) = resources.get("index.html.ejs") {
+        resources.insert("index.html",
+                         new_resource(
+                             index.data,
+                             index.modified,
+                             "text/html",
+                         ));
+    }
+
+    resources
+}
+
+pub fn generate_index_html(ui: &UI, template_file: String, branding_file_content: String) -> tera::Result<String> {
+    let template = template_file
+        .replace("<%=", "{{")
+        .replace("%>", "}}")
+        .replace(
+            "?? branding.application.title",
+            "| default(value=branding.application.title)",
+        )
+        .replace(
+            "?? branding.application.title",
+            "| default(value=branding.application.title)",
+        );
+
+    let env_json = serde_json::to_string(&ui)?;
+    let env_base64 = BASE64_STANDARD.encode(env_json.as_bytes());
+
+    let branding: Value = serde_json::from_str(&branding_file_content)?;
+
+    let mut context = tera::Context::new();
+    context.insert("_env", &env_base64);
+    context.insert("branding", &branding);
+
+    tera::Tera::one_off(&template, &context, true)
+}
+
+pub fn trustify_ui(ui: &UI) -> Result<HashMap<&'static str, Resource>, ()> {
+    let mut resources = generate();
+
+    let template_file = resources.get("index.html.ejs");
+    let branding_file_content = resources.get("branding/strings.json");
+
+
+    let index_html = INDEX_HTML.get_or_init(
+        || {
+            if let (Some(template_file), Some(branding_file_content)) = (template_file, branding_file_content) {
+                let template_file = from_utf8(template_file.data).map_err(|_| ()).unwrap();
+                let branding_file_content = from_utf8(branding_file_content.data).map_err(|_| ()).unwrap();
+                generate_index_html(ui, template_file.to_string(), branding_file_content.to_string()).unwrap()
+            } else {
+                "Something went wrong".to_string()
+            }
+        }
+    );
+
+    resources.insert("index.html",
+                     new_resource( index_html.as_bytes(), 0, "text/html", ) );
+
+    Ok(resources)
+}
+
+static INDEX_HTML: OnceLock<String> = OnceLock::new();
