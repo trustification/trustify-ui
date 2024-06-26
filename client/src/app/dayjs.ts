f19cebf56d9f652ed7d6b742da91278e0450ae1d
Gilles Dubreuil <gdubreui@redhat.com>
2024-06-26 10:06:14 +0200

    Import local dayjs declarations

--- client/src/app/dayjs.ts
+++ client/src/app/dayjs.ts
@@ -3,12 +3,10 @@ import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 import customParseFormat from "dayjs/plugin/customParseFormat";
-import relativeTime from "dayjs/plugin/relativeTime";
-import localizedFormat from "dayjs/plugin/localizedFormat";
+import arraySupport from "dayjs/plugin/arraySupport";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(customParseFormat);
 dayjs.extend(isSameOrBefore);
-dayjs.extend(relativeTime);
-dayjs.extend(localizedFormat);
+dayjs.extend(arraySupport);
