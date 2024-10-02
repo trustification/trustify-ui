9106523b7dbb6ca96dc9efc89e4110af7f1695dd
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2024-10-02 11:53:36 +0200

    Remove product page&routing as it won't be included in the release of rhtpa

--- client/src/app/layout/sidebar.tsx
+++ client/src/app/layout/sidebar.tsx
@@ -33,17 +33,7 @@ export const SidebarApp: React.FC = () => {
             >
               Search
             </NavLink>
-          </li>
-          <li className="pf-v5-c-nav__item">
-            <NavLink
-              to="/products"
-              className={({ isActive }) => {
-                return css(LINK_CLASS, isActive ? ACTIVE_LINK_CLASS : "");
-              }}
-            >
-              Products
-            </NavLink>
-          </li>
+          </li>         
           <li className="pf-v5-c-nav__item">
             <NavLink
               to="/sboms"
