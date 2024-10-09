7e2a9198260772c37f0f4978a479d52292365f94
Carlos Feria <carlosthe19916@gmail.com>
2024-10-09 14:16:04 +0200

    task: remove the Advisory main pages (#185)

--- client/src/app/layout/sidebar.tsx
+++ client/src/app/layout/sidebar.tsx
@@ -66,16 +66,6 @@ export const SidebarApp: React.FC = () => {
           </li>
           <li className="pf-v5-c-nav__item">
             <NavLink
-              to="/advisories"
-              className={({ isActive }) => {
-                return css(LINK_CLASS, isActive ? ACTIVE_LINK_CLASS : "");
-              }}
-            >
-              Advisories
-            </NavLink>
-          </li>
-          <li className="pf-v5-c-nav__item">
-            <NavLink
               to="/importers"
               className={({ isActive }) => {
                 return css(LINK_CLASS, isActive ? ACTIVE_LINK_CLASS : "");
