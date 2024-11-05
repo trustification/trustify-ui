e5e5a9c6b92ed689aca1af9714a2c525f0ec7a9b
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2024-11-05 13:34:08 +0100

    feat: add advisory list

---
+++ client/src/app/pages/advisory-list/advisory-context.tsx
@@ -0,0 +1,119 @@
+import React from "react";
+
+import { AxiosError } from "axios";
+
+import { AdvisorySummary } from "@app/client";
+import { FilterType } from "@app/components/FilterToolbar";
+import { TablePersistenceKeyPrefixes } from "@app/Constants";
+import {
+  getHubRequestParams,
+  ITableControls,
+  useTableControlProps,
+  useTableControlState,
+} from "@app/hooks/table-controls";
+import { useSelectionState } from "@app/hooks/useSelectionState";
+import { useFetchAdvisories } from "@app/queries/advisories";
+
+interface IAdvisorySearchContext {
+  tableControls: ITableControls<
+    AdvisorySummary,
+    "identifier" | "title" | "severity" | "revision" | "vulnerabilities",
+    "identifier" | "severity",
+    "" | "average_severity" | "revision",
+    string
+  >;
+
+  totalItemCount: number;
+  isFetching: boolean;
+  fetchError: AxiosError | null;
+}
+
+const contextDefaultValue = {} as IAdvisorySearchContext;
+
+export const AdvisorySearchContext =
+  React.createContext<IAdvisorySearchContext>(contextDefaultValue);
+
+interface IAdvisoryProvider {
+  children: React.ReactNode;
+}
+
+export const AdvisorySearchProvider: React.FunctionComponent<
+  IAdvisoryProvider
+> = ({ children }) => {
+  const tableControlState = useTableControlState({
+    tableName: "advisory",
+    persistenceKeyPrefix: TablePersistenceKeyPrefixes.advisories,
+    columnNames: {
+      identifier: "ID",
+      title: "Title",
+      severity: "Aggregated Severity",
+      revision: "Revision",
+      vulnerabilities: "Vulnerabilities",
+    },
+    isPaginationEnabled: true,
+    isSortEnabled: true,
+    sortableColumns: ["identifier", "severity"],
+    isFilterEnabled: true,
+    filterCategories: [
+      {
+        categoryKey: "",
+        title: "Filter text",
+        placeholderText: "Search",
+        type: FilterType.search,
+      },
+      {
+        categoryKey: "average_severity",
+        title: "Severity",
+        placeholderText: "Severity",
+        type: FilterType.multiselect,
+        selectOptions: [
+          { value: "none", label: "None" },
+          { value: "low", label: "Low" },
+          { value: "medium", label: "Medium" },
+          { value: "high", label: "High" },
+          { value: "critical", label: "Critical" },
+        ],
+      },
+      {
+        categoryKey: "revision",
+        title: "Revision",
+        type: FilterType.dateRange,
+      },
+    ],
+    isExpansionEnabled: false,
+  });
+
+  const {
+    result: { data: advisories, total: totalItemCount },
+    isFetching,
+    fetchError,
+  } = useFetchAdvisories(
+    getHubRequestParams({
+      ...tableControlState,
+      hubSortFieldKeys: {
+        identifier: "identifier",
+        severity: "average_score",
+      },
+    })
+  );
+
+  const tableControls = useTableControlProps({
+    ...tableControlState,
+    idProperty: "identifier",
+    currentPageItems: advisories,
+    totalItemCount,
+    isLoading: isFetching,
+    selectionState: useSelectionState({
+      items: advisories,
+      isEqual: (a, b) => a.identifier === b.identifier,
+    }),
+  });
+
+  return (
+    <AdvisorySearchContext.Provider
+      value={{ totalItemCount, isFetching, fetchError, tableControls }}
+    >
+      {children}
+    </AdvisorySearchContext.Provider>
+  );
+};
