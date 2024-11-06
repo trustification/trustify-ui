0c4c330e5c0cfd75f21df56edab335a0d00e390a
Rachel Yordán <rachelyordan@gmail.com>
2024-11-06 14:11:06 +0000

    add dummy stories for vuln details components (#210)

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,8 +4,10 @@ import "@patternfly/patternfly/utilities/Display/display.css";
 import "@patternfly/patternfly/utilities/Flex/flex.css";
 import "@patternfly/patternfly/utilities/Sizing/sizing.css";
 import "@patternfly/patternfly/utilities/Spacing/spacing.css";
+import React from "react";
 
 import type { Preview } from "@storybook/react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const preview: Preview = {
   parameters: {
