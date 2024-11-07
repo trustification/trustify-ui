88f7e5b65a43e70937a8aed250c0f0d321e41348
kahboom <rachelyordan@gmail.com>
2024-11-07 15:16:31 +0000

    fix(cve): cve list page enhancements
    fix: rm length
    
    fix: add join in case of multiple values

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,10 +4,7 @@ import "@patternfly/patternfly/utilities/Display/display.css";
 import "@patternfly/patternfly/utilities/Flex/flex.css";
 import "@patternfly/patternfly/utilities/Sizing/sizing.css";
 import "@patternfly/patternfly/utilities/Spacing/spacing.css";
-import React from "react";
-
 import type { Preview } from "@storybook/react";
-import { BrowserRouter as Router } from "react-router-dom";
 
 const preview: Preview = {
   parameters: {
