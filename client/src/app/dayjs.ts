dbc9c9d0d3586e82d2294fec40955f6226d3ecf7
Carlos Feria <carlosthe19916@gmail.com>
2024-11-07 13:54:46 +0100

    feat: pollish importers to v1 parity goals (#209)

--- client/src/app/dayjs.ts
+++ client/src/app/dayjs.ts
@@ -1,12 +1,16 @@
 import dayjs from "dayjs";
-import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
-import customParseFormat from "dayjs/plugin/customParseFormat";
 import arraySupport from "dayjs/plugin/arraySupport";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import duration from "dayjs/plugin/duration";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+import relativeTime from "dayjs/plugin/relativeTime";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(customParseFormat);
 dayjs.extend(isSameOrBefore);
 dayjs.extend(arraySupport);
+dayjs.extend(relativeTime);
+dayjs.extend(duration);
