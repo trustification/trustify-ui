cf0b944cd732790baec15d6bf4164109a8911761
kahboom <rachelyordan@gmail.com>
2024-11-22 17:06:58 +0000

    feat(story): advisory table

--- client/src/app/pages/advisory-list/advisory-table.stories.tsx
+++ client/src/app/pages/advisory-list/advisory-table.stories.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
+<<<<<<< HEAD
 import { AdvisoryTable } from "./advisory-table";
 import { AdvisorySearchContext } from "./advisory-context";
 import listResponse from "@mocks/data/advisory/list.json";
@@ -40,6 +41,26 @@ const tableControlsCustom = {
   isPaginationEnabled: true,
   isSortEnabled: true,
   sortableColumns: ["identifier", "severity", "modified"],
+=======
+import { BrowserRouter } from "react-router-dom";
+import { AdvisoryTable } from "./advisory-table";
+import { AdvisorySearchContext } from "./advisory-context";
+import listResponse from "@mocks/data/advisory/list.json";
+
+const tableControlsCustom = {
+  tableName: "advisory",
+  persistenceKeyPrefix: "vn",
+  columnNames: {
+    identifier: "ID",
+    title: "Description",
+    severity: "CVSS",
+    published: "Date published",
+    sboms: "Related documents",
+  },
+  isPaginationEnabled: true,
+  isSortEnabled: true,
+  sortableColumns: ["published", "sboms"],
+>>>>>>> c1bcfb8 (feat(story): advisory table)
   isFilterEnabled: true,
   filterCategories: [
     {
@@ -50,8 +71,13 @@ const tableControlsCustom = {
     },
     {
       categoryKey: "average_severity",
+<<<<<<< HEAD
       title: "Severity",
       placeholderText: "Severity",
+=======
+      title: "CVSS",
+      placeholderText: "CVSS",
+>>>>>>> c1bcfb8 (feat(story): advisory table)
       type: "multiselect",
       selectOptions: [
         {
@@ -76,6 +102,7 @@ const tableControlsCustom = {
         },
       ],
     },
+<<<<<<< HEAD
     {
       categoryKey: "modified",
       title: "Revision",
@@ -83,12 +110,21 @@ const tableControlsCustom = {
     },
   ],
   isExpansionEnabled: false,
+=======
+  ],
+  isExpansionEnabled: true,
+  expandableVariant: "compound",
+>>>>>>> c1bcfb8 (feat(story): advisory table)
   filterState: {
     filterValues: {},
   },
   sortState: {
     activeSort: {
+<<<<<<< HEAD
       columnKey: "identifier",
+=======
+      columnKey: "modified",
+>>>>>>> c1bcfb8 (feat(story): advisory table)
       direction: "asc",
     },
   },
@@ -111,11 +147,16 @@ const tableControlsCustom = {
       },
       {
         id: "title",
+<<<<<<< HEAD
         label: "Title",
+=======
+        label: "Description",
+>>>>>>> c1bcfb8 (feat(story): advisory table)
         isVisible: true,
       },
       {
         id: "severity",
+<<<<<<< HEAD
         label: "Aggregated Severity",
         isVisible: true,
       },
@@ -127,6 +168,19 @@ const tableControlsCustom = {
       {
         id: "vulnerabilities",
         label: "Vulnerabilities",
+=======
+        label: "CVSS",
+        isVisible: true,
+      },
+      {
+        id: "published",
+        label: "Date published",
+        isVisible: true,
+      },
+      {
+        id: "sboms",
+        label: "Related documents",
+>>>>>>> c1bcfb8 (feat(story): advisory table)
         isVisible: true,
       },
     ],
@@ -150,13 +204,17 @@ const tableControlsCustom = {
   },
   propHelpers: {
     toolbarProps: {
+<<<<<<< HEAD
       className: "",
+=======
+>>>>>>> c1bcfb8 (feat(story): advisory table)
       collapseListedFiltersBreakpoint: "xl",
       clearFiltersButtonText: "Clear all filters",
     },
     tableProps: {
       isExpandable: false,
     },
+<<<<<<< HEAD
     filterToolbarProps: {},
     paginationProps: {
       itemCount: 58,
@@ -171,6 +229,85 @@ const tableControlsCustom = {
     },
     toolbarBulkSelectorProps: {
       areAllSelected: false,
+=======
+    filterToolbarProps: {
+      filterCategories: [
+        {
+          categoryKey: "",
+          title: "Filter text",
+          placeholderText: "Search",
+          type: "search",
+        },
+        {
+          categoryKey: "average_severity",
+          title: "CVSS",
+          placeholderText: "CVSS",
+          type: "multiselect",
+          selectOptions: [
+            {
+              value: "none",
+              label: "None",
+            },
+            {
+              value: "low",
+              label: "Low",
+            },
+            {
+              value: "medium",
+              label: "Medium",
+            },
+            {
+              value: "high",
+              label: "High",
+            },
+            {
+              value: "critical",
+              label: "Critical",
+            },
+          ],
+        },
+      ],
+      filterValues: {},
+    },
+    filterPanelProps: {
+      filterCategories: [
+        {
+          categoryKey: "",
+          title: "Filter text",
+          placeholderText: "Search",
+          type: "search",
+        },
+        {
+          categoryKey: "average_severity",
+          title: "CVSS",
+          placeholderText: "CVSS",
+          type: "multiselect",
+          selectOptions: [
+            {
+              value: "none",
+              label: "None",
+            },
+            {
+              value: "low",
+              label: "Low",
+            },
+            {
+              value: "medium",
+              label: "Medium",
+            },
+            {
+              value: "high",
+              label: "High",
+            },
+            {
+              value: "critical",
+              label: "Critical",
+            },
+          ],
+        },
+      ],
+      filterValues: {},
+>>>>>>> c1bcfb8 (feat(story): advisory table)
     },
     getThProps: () => {},
     getTrProps: () => {},
@@ -178,13 +315,45 @@ const tableControlsCustom = {
   },
 };
 
+<<<<<<< HEAD
+=======
+const meta = {
+  title: "Components/AdvisoryList/AdvisoryTable",
+  component: AdvisoryTable,
+  tags: ["autodocs"],
+  decorators: [
+    (Story, { parameters }) => {
+      const { contextDefaultValue } = parameters;
+      return (
+        <BrowserRouter>
+          <AdvisorySearchContext.Provider value={contextDefaultValue}>
+            <Story />
+          </AdvisorySearchContext.Provider>
+        </BrowserRouter>
+      );
+    },
+  ],
+} satisfies Meta<typeof AdvisoryTable>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+>>>>>>> c1bcfb8 (feat(story): advisory table)
 export const PrimaryState: Story = {
   parameters: {
     contextDefaultValue: {
       isFetching: false,
+<<<<<<< HEAD
       fetchError: null,
       tableControls: { ...tableControlsCustom },
       totalItemCount: 58,
+=======
+      fetchError: "",
+      tableControls: { ...tableControlsCustom },
+      totalItemCount: 3,
+      currentPageItems: 10,
+      numRenderedColumns: 3,
+>>>>>>> c1bcfb8 (feat(story): advisory table)
     },
   },
 };
