84f5b5b565a1fcdafcc60cec5a69101d985ba439
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2025-01-07 14:56:25 +0100

    fix: links opened in new tabs should not redirect to base path

--- client/src/app/oidc.ts
+++ client/src/app/oidc.ts
@@ -11,7 +11,7 @@ export const OIDC_CLIENT_ID = ENV.OIDC_CLIENT_ID || "frontend";
 export const oidcClientSettings: OidcClientSettings = {
   authority: OIDC_SERVER_URL,
   client_id: OIDC_CLIENT_ID,
-  redirect_uri: window.location.origin,
+  redirect_uri: window.location.href,
   post_logout_redirect_uri: window.location.origin,
   response_type: "code",
   loadUserInfo: true,
