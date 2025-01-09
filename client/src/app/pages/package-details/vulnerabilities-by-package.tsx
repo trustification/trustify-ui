a91ad6dabc26e76581dddd423f5fad13a73d3595
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2025-01-09 17:04:09 +0100

    fix: package's vulnerabilities -> reduce number of calls

--- client/src/app/pages/package-details/vulnerabilities-by-package.tsx
+++ client/src/app/pages/package-details/vulnerabilities-by-package.tsx
@@ -8,12 +8,7 @@ import { Table, Tbody, Td, Th, Thead, Tr } from "@patternfly/react-table";
 
 import { getSeverityPriority } from "@app/api/model-utils";
 import { VulnerabilityStatus } from "@app/api/models";
-import { client } from "@app/axios-config/apiInit";
-import {
-  getVulnerability,
-  PurlAdvisory,
-  VulnerabilityDetails,
-} from "@app/client";
+import { PurlAdvisory, VulnerabilityDetails } from "@app/client";
 import { SeverityShieldAndText } from "@app/components/SeverityShieldAndText";
 import { SimplePagination } from "@app/components/SimplePagination";
 import {
@@ -22,8 +17,8 @@ import {
   TableRowContentWithControls,
 } from "@app/components/TableControls";
 import { VulnerabilityDescription } from "@app/components/VulnerabilityDescription";
+import { useVulnerabilitiesOfPackage } from "@app/hooks/domain-controls/useVulnerabilitiesOfPackage";
 import { useLocalTableControls } from "@app/hooks/table-controls";
-import { useFetchPackageById } from "@app/queries/packages";
 import { useWithUiId } from "@app/utils/query-utils";
 import { formatDate } from "@app/utils/utils";
 
@@ -42,101 +37,27 @@ export const VulnerabilitiesByPackage: React.FC<
   VulnerabilitiesByPackageProps
 > = ({ packageId }) => {
   const {
-    pkg,
-    isFetching: isFetchingPackage,
-    fetchError: fetchErrorPackage,
-  } = useFetchPackageById(packageId);
+    data: { vulnerabilities, summary },
+    isFetching: isFetchingVulnerabilities,
+    fetchError: fetchErrorVulnerabilities,
+  } = useVulnerabilitiesOfPackage(packageId);
 
-  const [allVulnerabilities, setAllVulnerabilities] = React.useState<
-    TableData[]
-  >([]);
-  const [vulnerabilitiesById, setVulnerabilitiesById] = React.useState<
-    Map<string, VulnerabilityDetails>
-  >(new Map());
-  const [isFetchingVulnerabilities, setIsFetchingVulnerabilities] =
-    React.useState(false);
-
-  React.useEffect(() => {
-    const vulnerabilities: TableData[] = (pkg?.advisories ?? [])
-      .flatMap((advisory) => {
-        return advisory.status.map((status) => {
-          const result: TableData = {
-            vulnerabilityId: status.vulnerability.identifier,
-            status: status.status as VulnerabilityStatus,
-            advisory: advisory,
-          };
-          return result;
-        });
-      })
-      // Take only "affected"
-      .filter((item) => item.status === "affected")
-      // Remove dupplicates if exists
-      .reduce((prev, current) => {
-        const exists = prev.find(
-          (item) =>
-            item.vulnerabilityId === current.vulnerabilityId &&
-            item.advisory.uuid === current.advisory.uuid
-        );
-        if (!exists) {
-          return [...prev, current];
-        } else {
-          return prev;
-        }
-      }, [] as TableData[]);
-
-    setAllVulnerabilities(vulnerabilities);
-    setIsFetchingVulnerabilities(true);
-
-    Promise.all(
-      vulnerabilities
-        .map(async (item) => {
-          const response = await getVulnerability({
-            client,
-            path: { id: item.vulnerabilityId },
-          });
-          return response.data;
-        })
-        .map((vulnerability) => vulnerability.catch(() => null))
-    ).then((vulnerabilities) => {
-      const validVulnerabilities = vulnerabilities.reduce((prev, current) => {
-        if (current) {
-          return [...prev, current];
-        } else {
-          // Filter out error responses
-          return prev;
-        }
-      }, [] as VulnerabilityDetails[]);
-
-      const vulnerabilitiesById = new Map<string, VulnerabilityDetails>();
-      validVulnerabilities.forEach((vulnerability) =>
-        vulnerabilitiesById.set(vulnerability.identifier, vulnerability)
-      );
-
-      setVulnerabilitiesById(vulnerabilitiesById);
-      setIsFetchingVulnerabilities(false);
-    });
-  }, [pkg]);
-
-  const tableData = React.useMemo(() => {
-    return allVulnerabilities.map((item) => {
-      const result: TableData = {
-        ...item,
-        vulnerability: vulnerabilitiesById.get(item.vulnerabilityId),
-      };
-      return result;
-    });
-  }, [allVulnerabilities, vulnerabilitiesById]);
+  const affectedVulnerabilities = React.useMemo(() => {
+    return vulnerabilities.filter(
+      (item) => item.vulnerabilityStatus === "affected"
+    );
+  }, [vulnerabilities]);
 
   const tableDataWithUiId = useWithUiId(
-    tableData,
-    (d) => `${d.vulnerabilityId}-${d.advisory.identifier}-${d.advisory.uuid}`
+    affectedVulnerabilities,
+    (d) => `${d.vulnerability.identifier}-${d.vulnerabilityStatus}`
   );
 
   const tableControls = useLocalTableControls({
     tableName: "vulnerability-table",
     idProperty: "_ui_unique_id",
     items: tableDataWithUiId,
-    isLoading: isFetchingPackage || isFetchingVulnerabilities,
+    isLoading: isFetchingVulnerabilities,
     columnNames: {
       identifier: "ID",
       description: "Description",
@@ -147,7 +68,7 @@ export const VulnerabilitiesByPackage: React.FC<
     isSortEnabled: true,
     sortableColumns: ["identifier", "severity", "published"],
     getSortValues: (item) => ({
-      identifier: item.vulnerabilityId,
+      identifier: item.vulnerability.identifier,
       severity: item.vulnerability?.average_severity
         ? getSeverityPriority(item.vulnerability?.average_severity)
         : 0,
@@ -203,8 +124,8 @@ export const VulnerabilitiesByPackage: React.FC<
           </Tr>
         </Thead>
         <ConditionalTableBody
-          isLoading={isFetchingPackage || isFetchingVulnerabilities}
-          isError={!!fetchErrorPackage}
+          isLoading={isFetchingVulnerabilities}
+          isError={!!fetchErrorVulnerabilities}
           isNoData={tableDataWithUiId.length === 0}
           numRenderedColumns={numRenderedColumns}
         >
@@ -218,8 +139,10 @@ export const VulnerabilitiesByPackage: React.FC<
                     rowIndex={rowIndex}
                   >
                     <Td width={15} {...getTdProps({ columnKey: "identifier" })}>
-                      <Link to={`/vulnerabilities/${item.vulnerabilityId}`}>
-                        {item.vulnerabilityId}
+                      <Link
+                        to={`/vulnerabilities/${item.vulnerability.identifier}`}
+                      >
+                        {item.vulnerability.identifier}
                       </Link>
                     </Td>
                     <Td
