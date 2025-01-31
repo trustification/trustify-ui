647f1aec95564d37dfcc3e11080346729f11d410
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2025-01-31 21:30:48 +0100

    Add branding assets

--- client/vite.config.common.ts
+++ client/vite.config.common.ts
@@ -2,6 +2,16 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import path from "path";
 
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const { brandingAssetPath } = require("@trustify-ui/common");
+
+const brandingPath: string = brandingAssetPath();
+const manifestPath = path.resolve(brandingPath, "manifest.json");
+const faviconPath = path.resolve(brandingPath, "favicon.ico");
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -11,7 +21,23 @@ export default defineConfig({
       transform(code) {
         return code.replace(/process\.env/g, '({})');
       },
-    }
+    },
+    viteStaticCopy({
+      targets: [
+        {
+          src: manifestPath,
+          dest: '.'
+        },
+        {
+          src: brandingPath,
+          dest: ".",
+        },
+        {
+          src: faviconPath,
+          dest: ".",
+        },
+      ]
+    }),
   ],
   resolve: {
     alias: {
