aadc87ba10291924e23accfe8dec362841c7c582
Carlos Feria <carlosthe19916@gmail.com>
2025-06-11 16:05:16 +0200

    :sparkles: SBOM|Advisory labels (#482)
    Signed-off-by: Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>

---
+++ client/src/app/components/FilterPanel/AutocompleteLabelFilterControl.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+
+import { getString } from "@app/utils/utils";
+
+import { Autocomplete } from "../Autocomplete/Autocomplete";
+import type { AutocompleteOptionProps } from "../Autocomplete/type-utils";
+import type {
+  FilterSelectOptionProps,
+  IMultiselectFilterCategory,
+} from "../FilterToolbar";
+import type { IFilterControlProps } from "./FilterControl";
+
+export interface IAutocompleteLabelFilterControlProps<TItem>
+  extends IFilterControlProps<TItem, string> {
+  category: IMultiselectFilterCategory<TItem, string>;
+}
+
+export const AutocompleteLabelFilterControl = <TItem,>({
+  category,
+  filterValue,
+  setFilterValue,
+  isDisabled = false,
+}: React.PropsWithChildren<
+  IAutocompleteLabelFilterControlProps<TItem>
+>): JSX.Element | null => {
+  const [selectOptions, setSelectOptions] = React.useState<
+    FilterSelectOptionProps[]
+  >(Array.isArray(category.selectOptions) ? category.selectOptions : []);
+
+  React.useEffect(() => {
+    setSelectOptions(
+      Array.isArray(category.selectOptions) ? category.selectOptions : [],
+    );
+  }, [category.selectOptions]);
+
+  return (
+    <Autocomplete
+      showChips
+      isDisabled={isDisabled}
+      options={selectOptions.map((option) => ({
+        id: option.value,
+        name: option.label ?? option.value,
+      }))}
+      selections={filterValue?.map((value) => {
+        const option: AutocompleteOptionProps = {
+          id: value,
+          name: value,
+        };
+        return option;
+      })}
+      onChange={(selections) => {
+        const newFilterValue = selections.map((option) =>
+          getString(option.name),
+        );
+        setFilterValue(newFilterValue);
+      }}
+      noResultsMessage="No search results"
+      placeholderText={category.placeholderText}
+      searchInputAriaLabel="select-autocomplete-listbox"
+      onSearchChange={category.onInputValueChange}
+    />
+  );
+};
